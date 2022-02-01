26ad048c98a1cf74dc5fe19ae5ab69b98974c6dd
Rafael Alcides <rafaelalci10@gmail.com>
2022-02-01 19:34:25 -0300

    Atividade numero 4 com o conceito de hot reload

---
+++ exercicio_4/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    
+    home: Card(
+      
+      child: Center(
+        child: App(
+        ),
+      ),
+    ),
+  ));
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+       
+      home: Card(
+       color: Colors.pinkAccent,
+        child: Center(
+          child: Text("Flutter",
+          style: TextStyle(color: Colors.white, fontSize: 32.0),
+          )
+        ),
+      ),
+    );
+  }
+}
